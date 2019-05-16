47973c77c0f49240ff986778551839df11c95633
Pavel Koshkin <koshkin.pasha@gmail.com>
2019-05-16 20:46:43 +0300

    use distance

--- main.sql
+++ main.sql
@@ -16,6 +16,7 @@ DROP TABLE IF EXISTS orders;
 
 --drop functions
 DROP FUNCTION IF EXISTS get_the_most_dangerous();
+DROP FUNCTION IF EXISTS distance(decimal, decimal, decimal, decimal);
 
 
 --create tables
@@ -104,7 +105,7 @@ BEGIN
     FROM (
         SELECT
             targets.enemy_id AS enemy_id,
-            SUM(defense_objects_types.importance * SQRT((targets.x - defense_objects.x) * (targets.x - defense_objects.x) + (targets.y - defense_objects.y) * (targets.y - defense_objects.y))) AS danger
+            SUM(defense_objects_types.importance * distance(targets.x, defense_objects.x, targets.y, defense_objects.y)) AS danger
         FROM targets, defense_objects
         INNER JOIN defense_objects_types
         ON defense_objects_types.defense_object_type_id = defense_objects.defense_object_type_id 
