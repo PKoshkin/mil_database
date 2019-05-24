07f10a6827d5d9b767a6bc1f3bdcb80993f26557
Pavel Koshkin <koshkin.pasha@gmail.com>
2019-05-24 11:23:43 +0300

    work ok

--- print.sql
+++ print.sql
@@ -1,15 +1,15 @@
---SELECT * FROM targets_with_danger LIMIT 10;
+SELECT * FROM targets_with_danger LIMIT 10;
 
---SELECT attack_the_most_dangerous();
---SELECT attack_the_most_dangerous();
---SELECT attack_the_most_dangerous();
---SELECT attack_the_most_dangerous();
+SELECT attack_the_most_dangerous();
+SELECT attack_the_most_dangerous();
+SELECT attack_the_most_dangerous();
+SELECT attack_the_most_dangerous();
 
---SELECT COUNT(*) FROM orders;
+SELECT COUNT(*) FROM orders;
 
---SELECT execute_orders();
+SELECT execute_orders();
 
---SELECT COUNT(*) FROM orders;
+SELECT COUNT(*) FROM orders;
 
 SELECT * FROM targets_with_danger LIMIT 10;
 SELECT * FROM charge_stocks LIMIT 10;
